f3a64d12499f66cec186537b3462411f1145f6c4
Jacob Fielding <jf0517@gmail.com>
2024-02-29 07:36:00 -0800

    Reverted to final publish

--- scripts/write_xcframework_spm.sh
+++ scripts/write_xcframework_spm.sh
@@ -9,6 +9,9 @@ release_url="https://github.com/Rallista/valhalla-mobile/releases/download/$GITH
 echo "Checksum: ${xcframework_checksum}"
 echo "Release URL: ${release_url}"
 
+# Replace `let useLocalBinary = true` line with `let useLocalBinary = false` in Package.swift
+sed -i '' "s|^let useLocalBinary: Bool = .*|let useLocalBinary: Bool = false|" Package.swift
+
 # Replace `let binaryURL` line with the new binary url for the release artifact in Package.swift
 sed -i '' "s|^let binaryURL: String = .*|let binaryURL: String = \"$release_url\"|" Package.swift
 
