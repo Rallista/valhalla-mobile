3a8e49d362cac2f243f924d3d06ce4b3b02b4081
Jacob Fielding <jf0517@gmail.com>
2024-03-03 09:55:27 -0800

    More fixes for CI release issues

--- scripts/write_xcframework_spm.sh
+++ scripts/write_xcframework_spm.sh
@@ -1,18 +1,20 @@
 
 # Get the zip file of the xcframework from ci   
 release_url=$1
+xcframework_zip="valhalla-wrapper.xcframework.zip"
 
 # Get the checksum of the xcframework file.
 xcframework_checksum=$(shasum -a 256 ${xcframework_zip} | awk '{print $1}')
+artifact_url = "${release_url}/${xcframework_file}"
 
 echo "Checksum: ${xcframework_checksum}"
-echo "Release URL: ${release_url}"
+echo "Release URL: ${artifact_url}"
 
 # Replace `let useLocalBinary = true` line with `let useLocalBinary = false` in Package.swift
 # sed -i '' "s|^let useLocalBinary: Bool = .*|let useLocalBinary: Bool = false|" Package.swift
 
 # Replace `let binaryURL` line with the new binary url for the release artifact in Package.swift
-sed -i '' "s|^let binaryURL: String = .*|let binaryURL: String = \"$release_url\"|" Package.swift
+sed -i '' "s|^let binaryURL: String = .*|let binaryURL: String = \"$artifact_url\"|" Package.swift
 
 # Replace let binaryChecksum: String? = nil with the new binary checksum for the release artifact in Package.swift
 sed -i '' "s|^let binaryChecksum: String = .*|let binaryChecksum: String = \"$xcframework_checksum\"|" Package.swift
