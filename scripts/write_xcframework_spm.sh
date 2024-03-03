182a16f1c71e48a36b4f5c75d1feed7e7fe9052b
Jacob Fielding <jf0517@gmail.com>
2024-03-02 19:23:47 -0800

    Working on release artifact path

--- scripts/write_xcframework_spm.sh
+++ scripts/write_xcframework_spm.sh
@@ -9,7 +9,7 @@ echo "Checksum: ${xcframework_checksum}"
 echo "Release URL: ${release_url}"
 
 # Replace `let useLocalBinary = true` line with `let useLocalBinary = false` in Package.swift
-sed -i '' "s|^let useLocalBinary: Bool = .*|let useLocalBinary: Bool = false|" Package.swift
+# sed -i '' "s|^let useLocalBinary: Bool = .*|let useLocalBinary: Bool = false|" Package.swift
 
 # Replace `let binaryURL` line with the new binary url for the release artifact in Package.swift
 sed -i '' "s|^let binaryURL: String = .*|let binaryURL: String = \"$release_url\"|" Package.swift
