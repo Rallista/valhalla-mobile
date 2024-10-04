067dbd76880ae3bcb66dc81a42df73e7db47c1f5
Jacob Fielding <jf0517@gmail.com>
2024-10-04 10:09:03 -0700

    WIP: getting vcpkg builds operational

--- scripts/build_android_vcpkg.sh
+++ scripts/build_android_vcpkg.sh
@@ -1,27 +1,59 @@
-#!/bin/sh
+#!/bin/zsh
 
 # TODO: Remove
 rm -rf build
 
-# TODO: This triplet can be changed for all iOS, Android, etc builds.
-# export ARCH=arm64-android
-# export ARCH=x64-android
+export VCPKG_ROOT=`pwd`/vcpkg
 
-echo "set(VCPKG_BUILD_TYPE release)" >> vcpkg/triplets/$ARCH.cmake
-export BUILD_DIR=`pwd`/build/android/$ARCH/wrapper
-export WRAPPER_DIR=`pwd`/src
-export TOOLCHAIN_FILE=`pwd`/vcpkg/scripts/buildsystems/vcpkg.cmake
+#
+# 1. Check the presence of required environment variables
+#
+if [ -z ${ANDROID_NDK_HOME+x} ]; then
+  echo "Please set ANDROID_NDK_HOME"
+  exit 1
+fi
+# if [ -z ${VCPKG_ROOT+x} ]; then
+#   echo "Please set VCPKG_ROOT"
+#   exit 1
+# fi
+
+#
+# 2. Set the path to the toolchains
+#
+vcpkg_toolchain_file=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake
+android_toolchain_file=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake
+
+#
+# 3. Select a pair "Android abi" / "vcpkg triplet"
+# Uncomment one of the four possibilities below
+#
+
+# android_abi=armeabi-v7a
+# vcpkg_target_triplet=arm-android
+
+# android_abi=x86
+# vcpkg_target_triplet=x86-android
+
+android_abi=arm64-v8a
+vcpkg_target_triplet=arm64-android
+
+# android_abi=x86_64
+# vcpkg_target_triplet=x64-android
+
+export BUILD_DIR=`pwd`/build/android/$android_abi/wrapper
+wrapper_dir=`pwd`/src
 
 # Move to the build directory
 mkdir -p $BUILD_DIR && cd $BUILD_DIR
 
 # vcpkg will install everything during cmake configuration
-# if you want to ENABLE_SERVICES=ON, install https://github.com/kevinkreiser/prime_server#build-and-install (no Windows)
-cmake -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE \
+cmake -DCMAKE_TOOLCHAIN_FILE=$vcpkg_toolchain_file \
+    -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$android_toolchain_file \
+    -DVCPKG_TARGET_TRIPLET=$vcpkg_target_triplet \
+    -DANDROID_ABI=$android_abi \
     -DENABLE_TOOLS=OFF -DENABLE_DATA_TOOLS=OFF \
     -DENABLE_PYTHON_BINDINGS=OFF -DENABLE_HTTP=OFF \
     -DENABLE_SERVICES=OFF -DENABLE_TESTS=OFF \
-    -DENABLE_STATIC_LIBRARY_MODULES=ON \
-    -S $WRAPPER_DIR \
+    -S $wrapper_dir \
     -B .
 cmake --build . -- -j$(nproc)
--- scripts/build_apple_vcpkg.sh
+++ scripts/build_apple_vcpkg.sh
@@ -1,30 +1,59 @@
-#!/bin/sh
+#!/bin/zsh
 
 # TODO: Remove
 rm -rf build
 
-export SDK="iphoneos"
+export VCPKG_ROOT=`pwd`/vcpkg
 
-# TODO: This triplet can be changed for all iOS, Android, etc builds.
-export ARCH=arm64-ios
-# export ARCH=arm64-ios-simulator
-# TODO: x86-64?
+# Check for Xcode command line tools
+if ! command -v xcodebuild &> /dev/null; then
+    echo "Xcode command line tools not found. Please install Xcode."
+    exit 1
+fi
 
-echo "set(VCPKG_BUILD_TYPE release)" >> vcpkg/triplets/community/$ARCH.cmake
-export BUILD_DIR=`pwd`/build/apple/$ARCH/wrapper
-export WRAPPER_DIR=`pwd`/src
-export TOOLCHAIN_FILE=`pwd`/vcpkg/scripts/buildsystems/vcpkg.cmake
+# Set the path to the toolchains
+vcpkg_toolchain_file=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake
+vcpkg_triplet_overlay=$VCPKG_ROOT/triplets/community
+
+# Define the platforms and architectures
+declare -A platforms=(
+    ["iOS"]="OS64"
+    ["iOS_Simulator"]="SIMULATOR64"
+    ["watchOS"]="WATCHOS"
+    ["watchOS_Simulator"]="SIMULATOR_WATCHOS"
+    ["tvOS"]="TVOS"
+    ["tvOS_Simulator"]="SIMULATOR_TVOS"
+)
+
+platform="iphoneos"
+sdk="iphoneos"
+arch="arm64"
+
+vcpkg_target_triplet=arm64-ios
+
+build_dir=`pwd`/build/apple/$sdk/wrapper
+wrapper_dir=`pwd`/src
 
 # Move to the build directory
-mkdir -p $BUILD_DIR && cd $BUILD_DIR
+mkdir -p $build_dir && cd $build_dir
 
 # vcpkg will install everything during cmake configuration
-cmake -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE \
+# -DENABLE_BITCODE=OFF \
+# -DENABLE_ARC=ON \
+# -DENABLE_VISIBILITY=ON \
+# -DENABLE_STRICT_TRY_COMPILE=ON \
+cmake -DCMAKE_TOOLCHAIN_FILE=$vcpkg_toolchain_file \
+    -DVCPKG_TARGET_TRIPLET=$vcpkg_target_triplet \
+    -DVCPKG_OVERLAY_TRIPLETS=$vcpkg_triplet_overlay \
+    -DCMAKE_SYSTEM_NAME=iOS \
+    -DCMAKE_OSX_SYSROOT=$sdk \
+    -DCMAKE_OSX_ARCHITECTURES=$arch \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=13.0 \
+    -DCMAKE_XCODE_ATTRIBUTE_ONLY_ACTIVE_ARCH=NO \
     -DENABLE_TOOLS=OFF -DENABLE_DATA_TOOLS=OFF \
     -DENABLE_PYTHON_BINDINGS=OFF -DENABLE_HTTP=OFF \
     -DENABLE_SERVICES=OFF -DENABLE_TESTS=OFF \
-    -DENABLE_STATIC_LIBRARY_MODULES=ON \
-    -S $WRAPPER_DIR \
-    -B .
-# cmake --build . -- -j$(nproc)
-cmake --build . --config Release --target install -- -sdk $SDK
+    -S $wrapper_dir \
+    -B . \
+    -G Xcode
+cmake --build . --config Release -- -jobs $(sysctl -n hw.ncpu)
