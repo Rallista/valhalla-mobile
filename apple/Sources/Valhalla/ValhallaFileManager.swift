53b8319f6d3e9a6d5c6e75d6f601f7065f388619
Jacob Fielding <jf0517@gmail.com>
2024-10-13 21:10:06 -0700

    Cleanup and testing for iOS

--- apple/Sources/Valhalla/ValhallaFileManager.swift
+++ apple/Sources/Valhalla/ValhallaFileManager.swift
@@ -40,7 +40,7 @@ enum ValhallaFileManager {
         }
 
         let tzdataFileData = try Data(contentsOf: tzdataFileURL)
-        let libraryURL = libraryDir.appendingPathComponent("tzdata.tar")
+        let libraryURL = libraryDir.appendingPathComponent("tzdata")
 
         // Write the tar to Library/tzdata
         // TODO: We can create our own tar extract here if we want to avoid the dependency
