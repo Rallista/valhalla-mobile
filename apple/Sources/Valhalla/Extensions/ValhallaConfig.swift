db3283878fec290bacf69560b3ca7a2861b15ffe
Jacob Fielding <jf0517@gmail.com>
2025-01-25 21:07:42 -0800

    Added public to valhalla config inits

--- apple/Sources/Valhalla/Extensions/ValhallaConfig.swift
+++ apple/Sources/Valhalla/Extensions/ValhallaConfig.swift
@@ -3,25 +3,54 @@ import ValhallaConfigModels
 
 extension ValhallaConfig {
     
-    init(data: Data) throws {
+    /// Initialize ValhallaConfig directly from JSON Data.
+    ///
+    /// Important! Using this method, you must have functional valhalla config json with
+    /// the correct local path to a tiles folder or tar inside the json.
+    ///
+    /// The ``init(tileExtractTar:)`` or ``init(tilesDir:)`` automate this process
+    /// by setting the path in the default valhalla json config (as produced by the valhalla build scripts).
+    ///
+    /// - Parameter data: A data representation of the valhalla config json.
+    public init(data: Data) throws {
         self = try JSONDecoder().decode(ValhallaConfig.self, from: data)
     }
 
-    init(_ json: String, using encoding: String.Encoding = .utf8) throws {
+    /// Initialize ValhallaConfig directly from a JSON string.
+    ///
+    /// Important! Using this method, you must have functional valhalla config json with
+    /// the correct local path to a tiles folder or tar inside the json.
+    ///
+    /// The ``init(tileExtractTar:)`` or ``init(tilesDir:)`` automate this process
+    /// by setting the path in the default valhalla json config (as produced by the valhalla build scripts).
+    ///
+    /// - Parameters:
+    ///   - json: The valhalla config json string.
+    ///   - encoding: The json's encoding. Default's to utf8
+    public init(_ json: String, using encoding: String.Encoding = .utf8) throws {
         guard let data = json.data(using: encoding) else {
             throw NSError(domain: "JSONDecoding", code: 0, userInfo: nil)
         }
         try self.init(data: data)
     }
-
-    init(fromURL url: URL) throws {
+    
+    /// Initialize ValhallaConfig from a local file url.
+    ///
+    /// Important! Using this method, the file path must point to a functional valhalla config json with
+    /// the correct local path to a tiles folder or tar inside the json.
+    ///
+    /// The ``init(tileExtractTar:)`` or ``init(tilesDir:)`` automate this process
+    /// by setting the path in the default valhalla json config (as produced by the valhalla build scripts).
+    ///
+    /// - Parameter url: The path of the valhalla config json file.
+    public init(fromURL url: URL) throws {
         try self.init(data: try Data(contentsOf: url))
     }
     
-    /// Generate a ValhallaConfig using the defaults provided by the valhalla build scripts.
+    /// Initialize ValhallaConfig using the defaults provided by the valhalla build scripts.
     ///
     /// This can be used to manipulate select values as a var.
-    init() {
+    public init() {
         guard let configJsonURL = Bundle.module.url(forResource: "default", withExtension: "json") else {
             fatalError("ValhallaConfig default.json config resource not found in module bundle.")
         }
@@ -33,7 +62,12 @@ extension ValhallaConfig {
         }
     }
     
-    init(tileExtractTar: URL) throws {
+    /// Initialize ValhallaConfig for a specific tar file.
+    ///
+    /// This injects a tile tarball file path into the default valhalla config.
+    ///
+    /// - Parameter tileExtractTar: The local file path URL for the tiles tar file.
+    public init(tileExtractTar: URL) throws {
         let defaultConfig = ValhallaConfig.loadDefault()
         
         var mjolnir = defaultConfig.mjolnir
@@ -50,7 +84,12 @@ extension ValhallaConfig {
                   thor: defaultConfig.thor)
     }
     
-    init(tilesDir: URL) throws {
+    /// Initialize ValhallaConfig for a specific tiles directory.
+    ///
+    /// This injects a tile folder path into the default valhalla config.
+    ///
+    /// - Parameter tileExtractTar: The local folder path URL for the tiles directory.
+    public init(tilesDir: URL) throws {
         let defaultConfig = ValhallaConfig.loadDefault()
         
         var mjolnir = defaultConfig.mjolnir
