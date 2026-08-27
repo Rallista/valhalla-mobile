d5283d99b6776e2c0728361a7a756a88ceed302f
HarelM <harel.mazor@gmail.com>
2026-08-27 22:36:27 +0300

    Reduce min sdk to 24

--- android/valhalla/build.gradle.kts
+++ android/valhalla/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        minSdk = 26
+        minSdk = 24
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
